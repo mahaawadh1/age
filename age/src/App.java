21aa582cca0d52fe482342a7c878694917163960
Maha A S A Salem <83347@kfh.com>
2024-01-08 15:23:09 +0300

    task age

---
+++ age/src/App.java
@@ -0,0 +1,31 @@
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String args[]) {
+        System.out.print("Enter date of birth in YYYY-MM-DD format: ");
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        scanner.close();
+
+        LocalDate dob = LocalDate.parse(input);
+        // prints the age
+        System.out.println("You are " + calculateAge(dob) + " years old.");
+    }
+
+    // the method calculates the age
+    public static int calculateAge(LocalDate dob) {
+
+        LocalDate curDate = LocalDate.now();
+
+        if ((dob != null) && (curDate != null)) {
+            return Period.between(dob, curDate).getYears();
+        } else {
+            return 0;
+        }
+    }
+
+}
